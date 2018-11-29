7d5b5c8fa5b566cac6d38b2af15f182e629a4abf
Blake Glanville <ephedrandrox@conspiron.com>
2018-11-29 10:06:18 -0800

    Initial Commit

---
+++ folScreenSaver/folScreenSaverView.h
@@ -0,0 +1,13 @@
+//
+//  folScreenSaverView.h
+//  folScreenSaver
+//
+//  Created by Blake Glanville on 11/29/18.
+//  Copyright © 2018 Blake Glanville. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface folScreenSaverView : ScreenSaverView
+
+@end
---
+++ folScreenSaver/folScreenSaverView.m
@@ -0,0 +1,52 @@
+//
+//  folScreenSaverView.m
+//  folScreenSaver
+//
+//  Created by Blake Glanville on 11/29/18.
+//  Copyright © 2018 Blake Glanville. All rights reserved.
+//
+
+#import "folScreenSaverView.h"
+
+@implementation folScreenSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
